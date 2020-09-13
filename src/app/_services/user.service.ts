a8ddc57a6a4b568e0601784e4114c5fd110c0274
Ujjwal Gupta <44123783+ujjwalgupta0909@users.noreply.github.com>
2020-09-14 03:00:17 +0530

    Update user.service.ts

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/';
+const API_URL = 'https://backend-pratilipi.herokuapp.com/api/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
